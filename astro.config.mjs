0089a77bdd9d99fbd3318736831d74e38ee7c925
SHAMIM REZA <85385535+shamimreza44@users.noreply.github.com>
2026-01-28 17:27:39 +0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astronaut.github.io',
+  site: 'https://shamimreza44.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
